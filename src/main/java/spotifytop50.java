3e4f11d92a3728d78ff294b4da64758f3e424fae
Lamdashi <155986030+LQ84i-1@users.noreply.github.com>
2024-02-15 09:22:18 -0500

    update documentation

--- src/main/java/spotifytop50.java
+++ src/main/java/spotifytop50.java
@@ -16,6 +16,8 @@ import java.util.List;
 
 /**
  * This class fetches the top 50 songs from a Spotify playlist and allows the user to access information about a song by its position in the playlist.
+ * We also used some resources from stack overflow, primarily, this link: https://stackoverflow.com/questions/2535723/try-catch-on-stack-overflows-in-java
+ * to use try, catch statements.
  */
 public class spotifytop50 {
 
@@ -25,18 +27,18 @@ public class spotifytop50 {
             SpotifyApi spotifyApi = authenticator.authenticate();  // trys to authetnitcate
             getTopSongs(spotifyApi); //uses the getTopSongs method
         } catch (IOException | SpotifyWebApiException | ParseException e) {
-            System.out.println("An error occurred: " + e.getMessage()); // prints an error message if anything goes wrong
+            System.out.println("We encountered an error: " + e.getMessage()); // prints an error message if anything goes wrong
         }
     }
 
     /**
-     * Fetches the top 50 songs from a Spotify playlist and allows the user to access information about a song by its position in the playlist.
+     * gets the top 50 songs the top 50 global Spotify playlist and allows the user to access information about a song by its position in the playlist.
      * @param spotifyApi The authenticated SpotifyApi instance.
      */
     private static void getTopSongs(SpotifyApi spotifyApi) {
-        String playlistId = "37i9dQZEVXbMDoHDwVN2tF"; // This is the ID for Spotify's Top 50 playlist
-        int limit = 50; // Maximum number of items to return per request (max is 100)
-        int offset = 0; // The index of the first item to return
+        String playlistId = "37i9dQZEVXbMDoHDwVN2tF"; // this is the ID for Spotify's Top 50 playlist
+        int limit = 50; // maximum number of items to return per request (max is 100)
+        int offset = 0; // the index of the first item to return
         List<String> songUrls = new ArrayList<>();
         List<String> songInfo = new ArrayList<>();
     
@@ -60,17 +62,17 @@ public class spotifytop50 {
                                              "\nPlaylist: " + spotifyApi.getPlaylist(playlistId).build().execute().getName();
                         System.out.println(songDetails);
 
-                        // Get the song's Spotify URL and add it to the list
+                        // get the song's Spotify URL and add it to the list
                         String spotifyUrl = track.getExternalUrls().get("spotify");
                         if (spotifyUrl != null) {
                             songUrls.add(spotifyUrl);
                             songInfo.add(songDetails + "\nURL: " + spotifyUrl);
                         }
                     } else {
-                        System.out.println("Invalid input. Please enter a number between 1 and 50.");
+                        System.out.println("Thats not a correct input. Please enter a number between 1 and 50.");
                     }
                 } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Please enter a number between 1 and 50.");
+                    System.out.println("Thats not a correct input. Please enter a number between 1 and 50.");
                 }
 
                 System.out.println("Do you want to search for another song? Enter 'yes' to continue or 'no' to stop.");
@@ -81,12 +83,12 @@ public class spotifytop50 {
             scanner.close();
 
             // After the loop, print the song info and URLs
-            System.out.println("Here is the information of the songs you selected:");
+            System.out.println("I have gathered the information of the songs you selected:");
             for (String info : songInfo) {
                 System.out.println(info);
             }
         } catch (IOException | SpotifyWebApiException | ParseException e) {
-            System.out.println("An error occurred: " + e.getMessage()); // if encoutnered these stuff, print the error message.
+            System.out.println("We encountered an error: " + e.getMessage()); // if encoutnered these stuff, print the error message.
         }
     }
 }
